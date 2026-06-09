3edf2717431450103985ab22bafdf562959cb4ae
shaheer786804-cloud <shaheer786804@gmail.com>
2026-06-09 13:34:52 +0500

    RandomWikipediaArticle.py

---
+++ RandomWikipediaArticle.py
@@ -0,0 +1,9 @@
+# python program to get a random wikipedia article
+def get_random_wikipedia_article():
+    url = "https://en.wikipedia.org/wiki/Special:Random"
+    print("\n\nClick the following url while holding the Ctrl key to open a random wikipedia article:")
+
+    print("\n ==========================================")
+    print(url)
+    print(" ==========================================\n\n")
+get_random_wikipedia_article()
